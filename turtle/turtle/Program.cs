7a6bd663d5a7f455de80810594ac7e6922fc8e8b
Iowan <dyakin_ip@outlook.com>
2016-07-18 22:42:16 +0300

    Starting TutrleGame

--- turtle/turtle/Program.cs
+++ turtle/turtle/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
 
 namespace turtle
 {
@@ -10,48 +11,7 @@ namespace turtle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("CineAdviser: Welcome!!!");
-            Console.WriteLine("What is your name?");
-            String name = Console.ReadLine();
-            Console.WriteLine("Hello, "+ name);
-
-            Console.WriteLine("What genre of film do you prefer: comedy or drama or fantstic or drama");
-            String genre = Console.ReadLine();
-            if (genre == "comedy")
-            {
-                Console.WriteLine("In this situation, i recommend you this title of films:");
-                Console.WriteLine("~ Operation 'I' and another advenure of Shurik");
-                Console.WriteLine("~ Back into the Future");
-                Console.WriteLine("~ Ivan Vasil'evic change profession");
-            }else if (genre == "drama")
-            {
-                Console.WriteLine("In this situation, i recommend you this title of films:");
-                Console.WriteLine("~ Green Mile");
-                Console.WriteLine("~ Hatico");
-                Console.WriteLine("~ 1+1");
-            } else if (genre == "fantastic")
-            {
-                Console.WriteLine("In this situation, i recommend you this title of films:");
-                Console.WriteLine("~ Autostop Galactic");
-                Console.WriteLine("~ Back into the Future");
-                Console.WriteLine("~ Star Way");
-            } else if (genre == "erotic")
-            {
-                Console.WriteLine("Please, count your ages:");
-                String YearSting = Console.ReadLine();
-                int year = Int32.Parse(YearSting);
-                if (year >= 18)
-                {
-                    Console.WriteLine("In this situation, i recommend you this title of films:");
-                    Console.WriteLine("Nimfomanka");
-                    Console.WriteLine("Sex-Drive");
-                } else
-                {
-                    Console.WriteLine("I can't recommend you any films of this genre!");
-                    Console.WriteLine("Keep into " + (18 - year) + " year/s.");
-                }
-            }
-            Console.Read();
+            Turtle.Move(100);  
         }
     }
 }
