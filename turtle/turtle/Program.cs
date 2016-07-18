1cf259671dcf03b393c1e2f1a35bd6fa9da433bd
Iowan <dyakin_ip@outlook.com>
2016-07-18 22:00:44 +0300

    CinemaAdviser Asking "drama or comedy"

--- turtle/turtle/Program.cs
+++ turtle/turtle/Program.cs
@@ -13,8 +13,23 @@ namespace turtle
             Console.WriteLine("CineAdviser: Welcome!!!");
             Console.WriteLine("What is your name?");
             String name = Console.ReadLine();
-            String text = "Hello, " + name;
-            Console.WriteLine(text);
+            Console.WriteLine("Hello, "+ name);
+
+            Console.WriteLine("What genre of film do you prefer: comedy or drama?");
+            String genre = Console.ReadLine();
+            if (genre == "comedy")
+            {
+                Console.WriteLine("In this situation, i recommend you this title of films:");
+                Console.WriteLine("~ Operation 'I' and another advenure of Shurik");
+                Console.WriteLine("~ Back into the Future");
+                Console.WriteLine("~ Ivan Vasil'evic change profession");
+            }else
+            {
+                Console.WriteLine("In this situation, i recommend you this title of films:");
+                Console.WriteLine("~ Green Mile");
+                Console.WriteLine("~ Hatico");
+                Console.WriteLine("~ 1+1");
+            }
             Console.Read();
         }
     }
